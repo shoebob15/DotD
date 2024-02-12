cfca46c155617caef3a42c78d1fe1d564597c35b
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-02-12 08:47:11 -0600

    change spritesheet, idk

--- core/src/com/shoebob/dotd/Player.java
+++ core/src/com/shoebob/dotd/Player.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Player { // TODO: Entity class
     // TODO: also better way of animating
-    protected int x = 0, y = 0;
+    protected int x = 0, y = 0, width = 64, height = 64;
     private float stateTime = 0f;
 
     Animation<TextureRegion> idleAnimation;
@@ -19,34 +19,33 @@ public class Player { // TODO: Entity class
 
     public Player() {
         idleSheet = new Texture(Gdx.files.internal("player_idle.png"));
-        idleAnimation = new Animation<TextureRegion>(0.1f, getIdleFrames());
-        walkSheet = new Texture(Gdx.files.internal("player_move.png"));
-        walkAnimation = new Animation<TextureRegion>(0.1f, getWalkFrames());
+        idleAnimation = new Animation<TextureRegion>(0.5f, getIdleFrames());
+        walkSheet = new Texture(Gdx.files.internal("player_walk_l.png"));
+        walkAnimation = new Animation<TextureRegion>(0.125f, getWalkFrames());
     }
 
     public void draw(SpriteBatch s) {
         TextureRegion frame = idleAnimation.getKeyFrame(stateTime, true);
         // TODO: PLayer moves faster on diagonal. Something something normalize vector?
-        // TODO: Probably use delta time, but womp womp
         if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
             if (Gdx.input.isKeyPressed(Input.Keys.W)) {
                 frame = walkAnimation.getKeyFrame(stateTime, true);
-                y += 5;
+                y += (int) (100 * Gdx.graphics.getDeltaTime());
             }
 
             if (Gdx.input.isKeyPressed(Input.Keys.S)) {
                 frame = walkAnimation.getKeyFrame(stateTime, true);
-                y -= 5;
+                y -= (int) (100 * Gdx.graphics.getDeltaTime());
             }
 
             if (Gdx.input.isKeyPressed(Input.Keys.A)) {
                 frame = walkAnimation.getKeyFrame(stateTime, true);
-                x -= 5;
+                x -= (int) (100 * Gdx.graphics.getDeltaTime());
             }
 
             if (Gdx.input.isKeyPressed(Input.Keys.D)) {
                 frame = walkAnimation.getKeyFrame(stateTime, true);
-                x += 5;
+                x += (int) (100 * Gdx.graphics.getDeltaTime());
             }
         } else {
             frame = idleAnimation.getKeyFrame(stateTime, true);
@@ -55,7 +54,7 @@ public class Player { // TODO: Entity class
 
         stateTime += Gdx.graphics.getDeltaTime();
 
-        s.draw(frame, x, y, 128, 128);
+        s.draw(frame, x, y, width, height);
     }
 
     public void dispose() {
@@ -63,17 +62,17 @@ public class Player { // TODO: Entity class
     }
 
     private TextureRegion[] getIdleFrames() {
-        TextureRegion[] tmp = new TextureRegion[4];
+        TextureRegion[] tmp = new TextureRegion[2];
         for (int i = 0; i < tmp.length; i++) {
-            tmp[i] = new TextureRegion(idleSheet, i * 32, 0, 32, 32);
+            tmp[i] = new TextureRegion(idleSheet, i * 16, 0, 16, 16);
         }
         return tmp;
     }
-    // TODO: Player shrinks while moving. Fix spritesheet file?
+
     private TextureRegion[] getWalkFrames() {
-        TextureRegion[] tmp = new TextureRegion[6];
+        TextureRegion[] tmp = new TextureRegion[2];
         for (int i = 0; i < tmp.length; i++) {
-            tmp[i] = new TextureRegion(walkSheet, i * 64, 0, 64, 64);
+            tmp[i] = new TextureRegion(walkSheet, i * 16, 0, 16, 16);
         }
         return tmp;
     }
