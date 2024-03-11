4c1d4fc74367de4dd557acca42c947bae9272e9e
Brennan Reinhard <brennanreinhard15@gmail.com>
2024-03-10 22:39:17 -0500

    big refactor coming up - make some components

---
+++ core/src/com/shoebob/dotd/components/Component.java
@@ -0,0 +1,8 @@
+package com.shoebob.dotd.components;
+
+public interface Component {
+    /*
+    Essentially a placeholder class for components to inherit from; simple OO enforcement
+    COMPONENTS SHOULD NOT HAVE METHODS, should be similar to structs, but java doesn't have them :(
+     */
+}
---
+++ core/src/com/shoebob/dotd/components/PositionComponent.java
@@ -0,0 +1,5 @@
+package com.shoebob.dotd.components;
+
+public class PositionComponent implements Component {
+    public float x = 0, y = 0;
+}
