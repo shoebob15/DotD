18f2d452c7ec1802f7a437fe10134ad5c5bb7e02
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-05-01 10:40:06 -0500

    what the sigma

--- core/src/com/shoebob/dotd/entities/attachments/ProjectileAttachment.java
+++ core/src/com/shoebob/dotd/entities/attachments/ProjectileAttachment.java
@@ -3,23 +3,25 @@ package com.shoebob.dotd.entities.attachments;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.shoebob.dotd.components.BodyComponent;
 import com.shoebob.dotd.components.PositionComponent;
 import com.shoebob.dotd.components.TextureComponent;
 import com.shoebob.dotd.components.VelocityComponent;
+import com.shoebob.dotd.entities.spells.SpellEntity;
 import com.shoebob.dotd.game.Consts;
 import com.shoebob.dotd.game.DotD;
 import com.shoebob.dotd.managers.ProjectileManager;
+import com.shoebob.dotd.managers.SpellEntityManager;
 import com.shoebob.dotd.spells.Spell;
+import com.shoebob.dotd.spells.SpellType;
 import com.shoebob.dotd.systems.VelocitySystem;
+import com.shoebob.dotd.util.Util;
 
 // a weapon that will summon projectile-type spells
 public class ProjectileAttachment extends Attachment {
-    public Spell spell;
-
-    public ProjectileAttachment(Spell spell, TextureComponent texture) {
+    public ProjectileAttachment(TextureComponent texture) {
         this.texture = texture;
         this.position = new PositionComponent();
-        this.spell = spell;
     }
 
     @Override
@@ -34,34 +36,42 @@ public class ProjectileAttachment extends Attachment {
         }
 
         if (cooldown()) {
-            VelocityComponent vel = new VelocityComponent();
+            if (game.player.attachmentInventory.selectedSpell.type == SpellType.SPELL_PROJECTILE) {
+                VelocityComponent vel = new VelocityComponent();
 
-            // calculate projectile velocity based off mouse position
-            vel.vector = new Vector2(Gdx.input.getX() - ((float) Gdx.graphics.getWidth() / 2),
-                    (Gdx.input.getY() - ((float) Gdx.graphics.getHeight() / 2)) * -1);
+                // calculate projectile velocity based off mouse position
+                vel.vector = new Vector2(Gdx.input.getX() - ((float) Gdx.graphics.getWidth() / 2),
+                        (Gdx.input.getY() - ((float) Gdx.graphics.getHeight() / 2)) * -1);
 
-            // normalize vector for uniform speed
-            vel.vector.nor();
+                // normalize vector for uniform speed
+                vel.vector.nor();
 
-            // multiply projectile velocity by a factor of x
-            vel.vector = VelocitySystem.mulVec(vel.vector, 2.5f);
+                // multiply projectile velocity by a factor of x
+                vel.vector = VelocitySystem.mulVec(vel.vector, 2.5f);
 
-            // put the velocity into the projectiles velocity component
-            spell.projectile.velocity = vel;
+                // put the velocity into the projectiles velocity component
+                game.player.attachmentInventory.selectedSpell.projectile.velocity = vel;
 
-            // set the projectiles position to the player
-            PositionComponent tmp = new PositionComponent();
-             tmp.x = game.player.position.x;
-             tmp.y = game.player.position.y;
-             spell.projectile.position = tmp;
+                // set the projectiles position to the player
+                PositionComponent tmp = new PositionComponent();
+                tmp.x = game.player.position.x;
+                tmp.y = game.player.position.y;
+                game.player.attachmentInventory.selectedSpell.projectile.position = tmp;
 
-            spell.projectile.animationComponent = game.player.attachmentInventory.selectedSpell.animation;
+                game.player.attachmentInventory.selectedSpell.projectile.animationComponent = game.player.attachmentInventory.selectedSpell.animation;
 
-            ProjectileManager.addAnimatedProjectile(spell.projectile, 5f);
+                ProjectileManager.addAnimatedProjectile(game.player.attachmentInventory.selectedSpell.projectile, 5f);
 
-            game.player.mana.currentMana -= game.player.attachmentInventory.selectedSpell.manaCost;
+                game.player.mana.currentMana -= game.player.attachmentInventory.selectedSpell.manaCost;
 
-            spell.projectile = spell.projectile.copy();
+                game.player.attachmentInventory.selectedSpell.projectile = game.player.attachmentInventory.selectedSpell.projectile.copy();
+            } else if (game.player.attachmentInventory.selectedSpell.type == SpellType.SPELL_POINT) {
+                SpellEntity entity = new SpellEntity(new PositionComponent(Util.screenToWorld(new PositionComponent(Gdx.input.getX(), Gdx.input.getY()), game)), new BodyComponent(8, 8), game.player.attachmentInventory.selectedSpell);
+
+                SpellEntityManager.addSpellEntity(entity);
+            } else {
+                throw new IllegalStateException("Unrecognized spell type.");
+            }
         }
     }
 
@@ -73,6 +83,6 @@ public class ProjectileAttachment extends Attachment {
 
         position = new PositionComponent(loc.x, loc.y);
 
-        spell = game.player.attachmentInventory.selectedSpell;
+        game.player.attachmentInventory.selectedSpell = game.player.attachmentInventory.selectedSpell;
     }
 }
