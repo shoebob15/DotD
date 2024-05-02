c57cc4cae29293527345eca9b0e9014e05ef6e29
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-05-02 07:47:03 -0500

    enemy projectile collisions

--- core/src/com/shoebob/dotd/managers/EnemyManager.java
+++ core/src/com/shoebob/dotd/managers/EnemyManager.java
@@ -1,6 +1,8 @@
 package com.shoebob.dotd.managers;
 
+import com.shoebob.dotd.components.BodyComponent;
 import com.shoebob.dotd.entities.enemies.EnemyEntity;
+import com.shoebob.dotd.entities.projectiles.AnimatedProjectile;
 import com.shoebob.dotd.entities.spells.SpellEntity;
 import com.shoebob.dotd.game.DotD;
 import com.shoebob.dotd.systems.LocationSystem;
@@ -14,6 +16,13 @@ public class EnemyManager {
         for (int i = 0 ; i < enemies.size(); i++) {
             for (SpellEntity entity : SpellEntityManager.getSpellEntities()) {
                 if (LocationSystem.bodyOverlaps(entity.position, entity.body, enemies.get(i).position, enemies.get(i).body)) {
+                    enemies.remove(i);
+                    i--;
+                }
+            }
+
+            for (AnimatedProjectile projectile : ProjectileManager.getAnimatedProjectiles()) {
+                if (LocationSystem.bodyOverlaps(projectile.position, new BodyComponent(projectile.animationComponent.animation.getKeyFrames()[0].getRegionWidth(), projectile.animationComponent.animation.getKeyFrames()[0].getRegionHeight()), enemies.get(i).position, enemies.get(i).body)) {
                     enemies.remove(i);
                     i--;
                 }
