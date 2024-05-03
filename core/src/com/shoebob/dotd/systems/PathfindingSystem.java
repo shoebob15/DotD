f7cfaf2f3f14875a5a60b14aab108c1ef95be661
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-05-03 08:09:27 -0500

    begin the pathfinding system

--- core/src/com/shoebob/dotd/systems/PathfindingSystem.java
+++ core/src/com/shoebob/dotd/systems/PathfindingSystem.java
@@ -1,4 +1,8 @@
 package com.shoebob.dotd.systems;
 
 public class PathfindingSystem {
+    // uses the a* pathfinding
+    public boolean[][] pathfind(boolean[][] map) {
+        return new boolean[1][1];
+    }
 }
