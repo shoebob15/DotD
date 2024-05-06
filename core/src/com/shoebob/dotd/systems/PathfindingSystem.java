e5fb27e9acc4deb9d36f81afc9b6d377890e8ff5
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-05-06 08:49:11 -0500

    pathfinding

--- core/src/com/shoebob/dotd/systems/PathfindingSystem.java
+++ core/src/com/shoebob/dotd/systems/PathfindingSystem.java
@@ -1,20 +1,20 @@
 package com.shoebob.dotd.systems;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PathfindingSystem {
+    private enum NodeType {
+        AIR,
+        WALL,
+        START,
+        END,
+        EXPLORED
+    }
 
-    static class Node {
-        enum NodeType {
-            AIR,
-            WALL,
-            START,
-            END,
-            EXPLORED
-        }
-
+    private class Node {
         public int x, y;
-        public double f; // cost of node
-        public double g; // distance between current node and start node
-        public double h; // heuristic | estimated distance from start to end node
+        public double distance; // calculated value of the distance
         public NodeType type;
         public Node parent;
 
@@ -28,20 +28,11 @@ public class PathfindingSystem {
             this.type = type;
         }
 
-        public void clearData() {
-            f = 0;
-            g = 0;
-            h = 0;
-        }
-
         @Override
         public String toString() {
             return "Node{" +
                     "x=" + x +
                     ", y=" + y +
-                    ", f=" + f +
-                    ", g=" + g +
-                    ", h=" + h +
                     ", type=" + type +
                     ", parent=" + parent +
                     '}';
@@ -50,7 +41,55 @@ public class PathfindingSystem {
 
 
     // uses the a* pathfinding
-    public static boolean[][] pathfind(boolean[][] map) {
+    public static boolean[][] pathfind(Node[][] map) {
+        Set<Node> unexplored = new HashSet<>(); // can not contain duplicate objects - "fancy" ArrayList
+
+        Node startNode = null;
+        Node endNode = null;
+        // reset each node
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] != null) {
+                    map[i][j].distance = Integer.MAX_VALUE;
+                    unexplored.add(map[i][j]);
+                }
+            }
+        }
+
+        // find starting node
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] != null && map[i][j].type == NodeType.START) {
+                    startNode = map[i][j];
+                }
+            }
+        }
+        // if the start node is not found, throw an exception
+        if (startNode == null) {
+            throw new IllegalArgumentException("The Node map must contain 1 start node.");
+        }
+
+        // find ending node
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] != null && map[i][j].type == NodeType.END) {
+                    endNode = map[i][j];
+                }
+            }
+        }
+        // if the end node is not found, throw an exception
+        if (endNode == null) {
+            throw new IllegalArgumentException("The Node map must contain 1 end node.");
+        }
+
+        while (!unexplored.isEmpty()) {
+
+        }
+
+        startNode.distance = 0;
+
+
+
         return new boolean[1][1];
     }
 }
